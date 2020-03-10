8d410e39b978c390ad61d0f73e41d2b350dc07ae
lileipeng <lileipeng@juren.com>
2020-03-10 21:01:28 +0800

    冒泡排序

---
+++ 03-BubbleSort.php
@@ -0,0 +1,25 @@
+<?php
+
+require "helper.php";
+
+// 冒泡排序
+function bubbleSort(array &$arr) {
+    $length = count($arr);
+
+    for ($i = $length -1; $i > 0; $i--){
+        $flag = false;
+        for($j = 0; $j < $i; $j++){
+            if($arr[$j] > $arr[$j + 1]){
+                swap($arr[$j], $arr[$j + 1]);
+                $flag = true;
+            }
+        }
+        if(!$flag){
+            break;
+        }
+    }
+}
+
+$n = 10;
+$arr = generateRandomArray($n, 0, 10);
+testSort("bubbleSort", $arr);
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [01-SelectionSort 选择排序](https://github.com/franklee0902/algorithm/blob/master/01-SelectionSort.php)
 
 [02-InsertionSort 插入排序](https://github.com/franklee0902/algorithm/blob/master/02-InsertionSort.php)
+
+[03-BubbleSort 冒泡排序](https://github.com/franklee0902/algorithm/blob/master/03-BubbleSort.php)
