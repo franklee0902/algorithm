0239517144def3aa4f94301db651fbb136cf0414
lileipeng <lileipeng@juren.com>
2020-03-13 00:19:31 +0800

    优化递归排序

--- 05-MergeSort.php
+++ 05-MergeSort.php
@@ -3,6 +3,18 @@
 require "helper.php";
 
 
+// 对arr[l...r]范围内的数据进行插入排序
+function __insertionSort(array &$arr, int $l, int $r) {
+    for($i = $l + 1; $i <= $r; $i++){
+        $e = $arr[$i];
+        $j = $i;
+        for(; $j > $l && $arr[$j - 1] > $e; $j--){
+            $arr[$j] = $arr[$j - 1];
+        }
+        $arr[$j] = $e;
+    }
+}
+
 // 将arr[l...mid]和arr[mid+1...r]两部分进行归并
 function __merge(array &$arr, int $l, int $mid, int $r) {
     // 使用辅助空间帮助归并
@@ -31,19 +43,24 @@ function __merge(array &$arr, int $l, int $mid, int $r) {
 // 递归使用归并排序，对arr[l...r]范围内的数据进行排序
 function __mergeSort(array &$arr, int $l, int $r) {
 
-//    // 小数据规模用插入排序做优化
-//    if($r - $l <= 15){
-//        insertionSort($arr, $l, $r);
+//    if($l >= $r)
 //        return;
-//    }
-    if($l >= $r)
+
+    // 小数据规模用插入排序做优化
+    if($r - $l <= 15){
+        __insertionSort($arr, $l, $r);
         return;
+    }
+
 
     // 防止溢出
     $mid = (int)(($r - $l) / 2) + $l;
     __mergeSort($arr, $l, $mid);
     __mergeSort($arr, $mid + 1, $r);
-    __merge($arr, $l, $mid, $r);
+
+    // 优化：如果两个区间本来就有序则不用归并
+    if($arr[$mid] > $arr[$mid + 1])
+        __merge($arr, $l, $mid, $r);
 }
 
 // 归并排序的时间复杂度是O(NlogN),空间复杂度是O(N)，因为在合并否时候需要额外的辅助空间，稳定排序
